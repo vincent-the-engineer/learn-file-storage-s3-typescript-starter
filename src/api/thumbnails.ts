bc8c1731a99ef5716f533537016b62c4003a7992
vincent-the-engineer <208263664+vincent-the-engineer@users.noreply.github.com>
2025-12-31 20:26:40 +1100

    Ch1 L5:

--- src/api/thumbnails.ts
+++ src/api/thumbnails.ts
@@ -1,9 +1,14 @@
 import { getBearerToken, validateJWT } from "../auth";
 import { respondWithJSON } from "./json";
-import { getVideo } from "../db/videos";
+import { getVideo, updateVideo } from "../db/videos";
 import type { ApiConfig } from "../config";
 import type { BunRequest } from "bun";
-import { BadRequestError, NotFoundError } from "./errors";
+import {
+  BadRequestError,
+  NotFoundError,
+  UserForbiddenError,
+} from "./errors";
+
 
 type Thumbnail = {
   data: ArrayBuffer;
@@ -11,6 +16,7 @@ type Thumbnail = {
 };
 
 const videoThumbnails: Map<string, Thumbnail> = new Map();
+const MAX_UPLOAD_SIZE = 10 << 20;  // 10MB
 
 export async function handlerGetThumbnail(cfg: ApiConfig, req: BunRequest) {
   const { videoId } = req.params as { videoId?: string };
@@ -42,12 +48,40 @@ export async function handlerUploadThumbnail(cfg: ApiConfig, req: BunRequest) {
     throw new BadRequestError("Invalid video ID");
   }
 
+  const formData = await req.formData();
+  const file = formData.get("thumbnail");
+  if (!(file instanceof File)) {
+    throw new BadRequestError("Thumbnail file missing");
+  }
+
+  if (file.size > MAX_UPLOAD_SIZE) {
+    throw new BadRequestError("Thumbnail is too large");
+  }
+
   const token = getBearerToken(req.headers);
   const userID = validateJWT(token, cfg.jwtSecret);
 
+  const video = getVideo(cfg.db, videoId);
+  if (!video) {
+    throw new NotFoundError("Couldn't find video");
+  }
+
+  if (video.userID !== userID) {
+    throw new UserForbiddenError("Forbidden");
+  }
+
+  const mediaType = file.type;
+  const arrayBuffer = await file.arrayBuffer();
+
   console.log("uploading thumbnail for video", videoId, "by user", userID);
 
-  // TODO: implement the upload here
+  videoThumbnails.set(videoId, {data: arrayBuffer, mediaType: mediaType});
 
-  return respondWithJSON(200, null);
+  const thumbnailURL = `http://localhost:${cfg.port}/api/thumbnails/${videoId}`;
+  video.thumbnailURL = thumbnailURL;
+
+  updateVideo(cfg.db, video);
+
+  return respondWithJSON(200, video);
 }
+
